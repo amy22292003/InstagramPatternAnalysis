f8580805f76764d18b805f981770ca81abfff512
amy22292003 <amy22292003@gmail.com>
2016-05-12 17:42:04 +0800

    Renamed Location Clustering files

--- LocationClustering/gps_locationfreq.py
+++ LocationClustering/gps_locationfreq.py
@@ -13,23 +13,23 @@ SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.
 sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
 sys.path.append(PACKAGE_PARENT)
 
+import Liu.custommodule.cluster as ccluster
 import Liu.custommodule.cpygmaps as cpygmaps
 import Liu.custommodule.fuzzycmeans as cfuzzy
 import Liu.custommodule.location as clocation
 import Liu.custommodule.user as cuser
 
 """parameters"""
+FILTER_TIME = 1443657600 # 2015/10/01 1448928000 # 2015/12/01
 CLUSTER_NUM = 30
 ERROR = 0.0001
-MAX_KTH = 20
-#CATEGORY_MIN_AMOUNT = 5
-#INTERSECT_THRESHOLD = 0.1 # the min intersection tags count% between the category locations and the candidate location
+MAX_KTH = 30
 
 """file path"""
 USER_POSTS_FILE = "./data/TravelerPosts"
-OUTPUT_MAP = "./data/LocationCluster/LocationMapCoor_klf_" + str(CLUSTER_NUM) +\
+OUTPUT_MAP = "./data/LocationCluster/LocationMapCoor_" + str(CLUSTER_NUM) +\
     "k" + str(MAX_KTH) + "e" + str(ERROR) + ".html"
-OUTPUT_CLUSTER = "./data/LocationCluster/LocationClusterCoor_klf_" + str(CLUSTER_NUM) +\
+OUTPUT_CLUSTER = "./data/LocationCluster/LocationClusterCoor_" + str(CLUSTER_NUM) +\
     "k" + str(MAX_KTH) + "e" + str(ERROR) + ".txt"
 
 def set_location_user_count(locations):
@@ -37,10 +37,7 @@ def set_location_user_count(locations):
         users = set(a_post.uid for a_post in locations[key].posts)
         setattr(locations[key], "usercount", len(users))
 
-
 def main():
-    """end for intersection clustering"""
-
     print("--------------------------------------")
     print("STARTTIME:", (datetime.datetime.now()))
     print("--------------------------------------")
@@ -48,8 +45,12 @@ def main():
     # getting data
     locations = clocation.open_locations()
     users = cuser.open_users_posts_afile(USER_POSTS_FILE)
+
+    print("Sampling users posts...")
+    for key, a_user in users.items():
+        posts = [x for x in a_user.posts if (x.time > FILTER_TIME) and (x.time < 1451606400)]
+        users[key].posts = posts
     locations = clocation.fit_users_to_location(locations, users, "uid")
-    del users
     set_location_user_count(locations)
 
     coordinate = numpy.array([(float(x.lat), float(x.lng)) for x in locations.values()])
@@ -59,17 +60,19 @@ def main():
     cntr, u, u0, d, jm, p, fpc, membership = cfuzzy.cmeans_coordinate(coordinate.T, CLUSTER_NUM, MAX_KTH, location_frequency, e=ERROR, algorithm="kthCluster_LocationFrequency")
     for i, key in enumerate(locations.keys()):
         setattr(locations[key], "cluster", membership[i])
+        setattr(locations[key], "membership", numpy.atleast_2d(u[:,i]))
 
     cpygmaps.output_clusters(\
         [(float(x.lat), float(x.lng), str(x.cluster) + " >> " + x.lname + " >>u:" + str(u[x.cluster, i])) for i, x in enumerate(locations.values())], \
         membership, CLUSTER_NUM, OUTPUT_MAP)
 
-    cfuzzy.output_location_cluster(locations.values(), "cluster", OUTPUT_CLUSTER)
-
+    ccluster.output_location_cluster(locations.values(), "cluster", OUTPUT_CLUSTER)
 
     print("--------------------------------------")
     print("ENDTIME:", (datetime.datetime.now()))
     print("--------------------------------------")
+
+    return users, locations
 
 if __name__ == '__main__':
     main()
--- LocationClustering/gps_tagintersection.py
+++ LocationClustering/gps_tagintersection.py
@@ -22,15 +22,15 @@ import Liu.custommodule.user as cuser
 CLUSTER_NUM = 30
 WEIGHT = 0.4
 MAX_KTH = 30
-ERROR = 0.001
+ERROR = 0.0001
 #CATEGORY_MIN_AMOUNT = 5
 #INTERSECT_THRESHOLD = 0.1 # the min intersection tags count% between the category locations and the candidate location
 
 """file path"""
 USER_POSTS_FILE = "./data/TravelerPosts"
-OUTPUT_MAP = "./data/Summary/LocationMap_k_" + str(CLUSTER_NUM) +\
+OUTPUT_MAP = "./data/Summary/LocationMapN_lf_" + str(CLUSTER_NUM) +\
     "k" + str(MAX_KTH) + "w" + str(WEIGHT) + "e" + str(ERROR) + ".html"
-OUTPUT_CLUSTER = "./data/Summary/LocationCluster_k_" + str(CLUSTER_NUM) +\
+OUTPUT_CLUSTER = "./data/Summary/LocationClusterN_lf_" + str(CLUSTER_NUM) +\
     "k" + str(MAX_KTH) + "w" + str(WEIGHT) + "e" + str(ERROR) + ".txt"
 
 
@@ -48,7 +48,7 @@ def get_tag_intersection(location_list):
     # output a square array(28xx * 28xx), x[i,j] : for location j, the propotion of the intersection with location i
     # x[i,j] != x[j,i] !!!
     print("Getting tags intersection...")
-    intersection = numpy.zeros((len(locations), len(locations)))
+    intersection = numpy.zeros((len(location_list), len(location_list)))
     for i, i_location in enumerate(location_list):
         if i % 100 == 0:
             print("  lopping, locations#", i)
@@ -65,11 +65,7 @@ def main():
 
     print("--------------------------------------")
     print("STARTTIME:", (datetime.datetime.now()))
-    print("--------------------------------------")
-    # setting cluster number
-    if len(sys.argv) > 1:
-        CLUSTER_NUM = int(sys.argv[1])
-
+    print("--------------------------------------")    
     # getting data
     locations = clocation.get_locations_list()
     users = cuser.get_users_posts_afile(USER_POSTS_FILE)
@@ -80,11 +76,21 @@ def main():
 
     coordinate = numpy.array([(float(x.lat), float(x.lng)) for x in locations.values()])
     intersection = get_tag_intersection(locations.values())
-    user_count = numpy.array([x.usercount for x in locations.values()])
-    print("avg usercount:", sum(user_count) / len(user_count), " max:", max(user_count), " min:", min(user_count))
+    location_frequency = numpy.array([x.usercount for x in locations.values()])
+    print("avg location_frequency:", sum(location_frequency) / len(location_frequency), " max:", max(location_frequency), " min:", min(location_frequency))
 
-    cntr1, u, u0, d1, d2, d, jm, p, fpc, membership = cfuzzy.cmeans_intersect(coordinate.T, intersection, CLUSTER_NUM,  MAX_KTH, w=WEIGHT, e=ERROR, algorithm="kthCluster")
-    #cntr1, u, u0, d1, d2, d, jm, p, fpc, membership = cfuzzy.cmeans_intersect(coordinate.T, intersection, CLUSTER_NUM,  MAX_KTH, user_count, w=WEIGHT, e=ERROR, algorithm="kthCluster_LocationFrequency")
+    print("location 1:", list(locations.values())[0].lname, list(locations.values())[0].lid)
+    print("intersection.sum:", intersection.sum(axis=0)[0:6], intersection.sum(axis=1)[0:6])
+    print("location_frequency:", location_frequency.shape)
+    # original intersect clustering
+    #cntr1, u, u0, d1, d2, d, jm, p, fpc, membership = cfuzzy.cmeans_intersect(coordinate.T, intersection, CLUSTER_NUM, w=WEIGHT, e=ERROR)
+    
+    # intersect clustering with the kth locations in each cluster
+    #cntr1, u, u0, d1, d2, d, jm, p, fpc, membership = cfuzzy.cmeans_intersect(coordinate.T, intersection, CLUSTER_NUM,  MAX_KTH, w=WEIGHT, e=ERROR, algorithm="kthCluster")
+    
+    # intersect clustering with the kth locations in each cluster & location frequency as weight
+    cntr1, u, u0, d1, d2, d, jm, p, fpc, membership = cfuzzy.cmeans_intersect(coordinate.T, intersection, CLUSTER_NUM,  MAX_KTH, location_frequency, w=WEIGHT, e=ERROR, algorithm="kthCluster_LocationFrequency")
+    
     for i, key in enumerate(locations.keys()):
         setattr(locations[key], "cluster", membership[i])
 
