[Fuzzy c means] corrected distance computation

custommodule/customskfuzzy/cluster/_cmeans_nocenter_i.py
@@ -80,13 +80,10 @@ def cmeans(data, c, m, error, maxiter, algorithm, level, *para, init = None, see
 	u0 = init
 	u = np.fmax(u0, np.finfo(np.float64).eps)
 
-	print("data id:", id(data))
-	print("para[1] id:", id(para[1]))
-
 	# Initialize loop parameters
 	jm = np.empty(0)
 	p = 0
-	print("u.shape:", u.shape, u.shape[0] * u.shape[1])
+	print("--u.shape:", u.shape, u.shape[0] * u.shape[1])
 
 	#select cmeans function
 	if algorithm is "2WeightedDistance":
@@ -98,7 +95,7 @@ def cmeans(data, c, m, error, maxiter, algorithm, level, *para, init = None, see
 	error_list = []
 	# Main cmeans loop
 	while p < maxiter - 1:
-		print("--", p, "----------------------------> ", datetime.datetime.now())
+		print("--", p, "-----------------------------------------> ", datetime.datetime.now())
 		u2 = u.copy()
 		[u, Jjm, d] = _cmeans0(data, u2, c, m, level, *para)
 		jm = np.hstack((jm, Jjm))

custommodule/customskfuzzy/cluster/distance.py
@@ -4,7 +4,7 @@ import numpy
 #from scipy.spatial.distance import cdist
 
 def _dist(u, v):
-    return math.sqrt(sum((u - v) ** 2))
+    return math.sqrt(((u - v) ** 2).sum())
 
 def _dynamic_programming(s1, s2):
     ml = numpy.ones([len(s1), len(s2)])
@@ -83,6 +83,6 @@ def get_distance(level, sequences, targets = None):
                     distance[i, j] = distance_func(s1, s2)
                 else:
                     distance[i, j] = distance[j, i]
-    print("-- [distance] max/min/mean/std:", distance.shape, numpy.amax(distance), numpy.amin(distance), numpy.mean(distance), numpy.std(distance))
-    print("-- [distance] ", datetime.datetime.now(), ">> spend:", datetime.datetime.now() - start_t)
+    print("   [distance] max/min/mean/std:", distance.shape, numpy.amax(distance), numpy.amin(distance), numpy.mean(distance), numpy.std(distance))
+    print("   [distance] ", datetime.datetime.now(), ">> spend:", datetime.datetime.now() - start_t)
     return distance