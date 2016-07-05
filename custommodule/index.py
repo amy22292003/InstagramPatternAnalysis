[Trajectory Clustering] - Transform trajectories into 3D - fixed similarity calculation error in vector distance

custommodule/index.py
@@ -48,14 +48,7 @@ def _center(level, u, k, seed=RAND_SEED_K):
     print("[Index] center:", large_k_indices)
     return large_k_indices
 
-def _cluster_dist(level, c, k, w, data1, data2, center):
-    #print("[index] Getting cluster distances")
-    d1 = _distance(level, c, k, data1, center)
-    d2 = _distance(level, c, k, data2, center)
-    d = w * d1 + (1 - w) * d2
-    return d
-
-def _distance(level, c, k, data, center):
+def _dist_c_c(level, c, k, data, center):
     center_data = np.array(data)[center]
     distance = cskfuzzy.cluster.distance.get_distance(level, center_data)
     #distance = distance / np.amax(distance)
@@ -68,7 +61,14 @@ def _distance(level, c, k, data, center):
     np.fill_diagonal(d, 0)
     return d
 
-def _distance_c_x(level, c, k, data, center):
+def _distance_c_c(level, c, k, w, data1, data2, center):
+    #print("[index] Getting cluster distances")
+    d1 = _dist_c_c(level, c, k, data1, center)
+    d2 = _dist_c_c(level, c, k, data2, center)
+    d = w * d1 + (1 - w) * d2
+    return d
+
+def _dist_c_x(level, c, k, data, center):
     center_data = np.array(data)[center]
     distance = cskfuzzy.cluster.distance.get_distance(level, data, center_data)
     #distance = distance / np.amax(distance)
@@ -78,18 +78,18 @@ def _distance_c_x(level, c, k, data, center):
     d = each_cluster.dot(distance) / k
     return d
 
-def _dist_c_x(level, c, k, w, data1, data2, center):
-    d1 = _distance_c_x(level, c, k, data1, center)
-    d2 = _distance_c_x(level, c, k, data2, center)
+def _distance_c_x(level, c, k, w, data1, data2, center):
+    d1 = _dist_c_x(level, c, k, data1, center)
+    d2 = _dist_c_x(level, c, k, data2, center)
     d = w * d1 + (1 - w) * d2
     return d
 
 def _j(level, u, k, w, data1, data2, center):
-    d = _dist_c_x(level, u.shape[0], k, w, data1, data2, center)
+    d = _distance_c_x(level, u.shape[0], k, w, data1, data2, center)
     return ((u ** 2) * (d ** 2)).sum()
 
 def _comp(level, u, k, w, data1, data2, center):
-    d = _dist_c_x(level, u.shape[0], k, w, data1, data2, center)
+    d = _distance_c_x(level, u.shape[0], k, w, data1, data2, center)
     return ((u ** 2) * (d ** 2)).sum()
 
 def _sep(u):
@@ -104,7 +104,7 @@ def pcaes(level, u, k, w, data1, data2):
     compactness = _total_compactness(u)
     
     center = _center(level, u, k)
-    d = _cluster_dist(level, u.shape[0], k, w, data1, data2, center)
+    d = _distance_c_c(level, u.shape[0], k, w, data1, data2, center)
     separation = _total_separation(d)
     result = compactness - separation
     print("-- compactness:", compactness, ", separation:", separation)
@@ -120,17 +120,16 @@ def npc(u):
 def xb(level, u, k, w, data1, data2):
     center = _center(level, u, k)
     j = _j(level, u, k, w, data1, data2, center)
-    cd = _cluster_dist(level, u.shape[0], k, w, data1, data2, center)
+    cd = _distance_c_c(level, u.shape[0], k, w, data1, data2, center)
     cd[cd == 0] = float('inf')
-    print("   cd max/min/mean/std:", cd.shape, np.amax(cd), np.amin(cd), np.mean(cd), np.std(cd))
     xb = (j / u.shape[1]) / np.amin(cd ** 2)
     return xb, j / u.shape[1], np.amin(cd ** 2)
 
 def bsc(level, u, k, w, data1, data2):
     center = _center(level, u, k)
-    d = _dist_c_x(level, u.shape[0], k, w, data1, data2, center)
+    d = _distance_c_x(level, u.shape[0], k, w, data1, data2, center)
     up = ((u ** 2) * (d ** 2)).sum(axis=1)
-    cd = _cluster_dist(level, u.shape[0], k, w, data1, data2, center)
+    cd = _distance_c_c(level, u.shape[0], k, w, data1, data2, center)
     down = u.sum(axis=1) * (cd ** 2).sum(axis=1)
     bsc = (up / down).sum()
     return bsc